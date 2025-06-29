758fc7e17ed2ea364ecb3c64f2b2c26b636edb3c
Dave Brown <dave.brown@drjtechnology.co.uk>
2025-06-29 12:11:50 +0100

    Add 'Totals' option to sales and stock reports
    - Updated `Report_Sales.sql` to include a 'Totals' report option when `@VenueId` is -1, allowing aggregation of sales data across all venues.
    - Modified `Report_Stock.sql` to add a similar 'Totals' option using a CTE for consolidated activity data.
    - Enhanced `ReportFilter.razor` with a new '-- Totals --' option in the venue filter dropdown.
    - Updated `appsettings.json` version from "1.1.0" to "1.2.0".
    - Adjusted `docker-compose.yml` to use the latest image version for the `stockmanagement` service.

--- StockManagement.Database/dbo/Stored Procedures/Report_Sales.sql
+++ StockManagement.Database/dbo/Stored Procedures/Report_Sales.sql
@@ -3,6 +3,8 @@
 -- Create date: 27 May 2025
 -- Description:	Get Sales
 -- =========================================================
+-- 29 JUN 2025 - Dave Brown - Include 'Totals' option
+-- =========================================================
 CREATE PROCEDURE [dbo].[Report_Sales]
 	@VenueId int = 0,
 	@ProductId int = 0,
@@ -12,18 +14,36 @@ BEGIN
 	SET NOCOUNT ON
 	DECLARE @Err int
 
-	SELECT v.VenueName, pt.ProductTypeName, p.ProductName, SUM(a.Quantity) AS TotalSales
-	FROM dbo.Activity a
-	INNER JOIN dbo.Product p ON a.ProductId = p.Id
-	INNER JOIN dbo.ProductType pt ON a.ProductTypeId = pt.Id
-	INNER JOIN dbo.Venue v ON a.VenueId = v.Id
-	WHERE	a.Deleted = 0
-		AND	a.ActionId = 5 -- Sale
-		AND (@VenueId = 0 OR a.VenueId = @VenueId)
-		AND (@ProductId = 0 OR a.ProductId = @ProductId)
-		AND (@ProductTypeId = 0 OR a.ProductTypeId = @ProductTypeId)
-	GROUP BY v.VenueName, pt.ProductTypeName, p.ProductName
-	ORDER BY v.VenueName, pt.ProductTypeName, p.ProductName
+	IF @VenueId = -1 -- Totals
+	BEGIN
+		SELECT 'Totals', pt.ProductTypeName, p.ProductName, SUM(a.Quantity) AS TotalSales
+		FROM dbo.Activity a
+		INNER JOIN dbo.Product p ON a.ProductId = p.Id
+		INNER JOIN dbo.ProductType pt ON a.ProductTypeId = pt.Id
+		INNER JOIN dbo.Venue v ON a.VenueId = v.Id
+		WHERE	a.Deleted = 0
+			AND	a.ActionId = 5 -- Sale
+			--AND (@VenueId = 0 OR a.VenueId = @VenueId)
+			AND (@ProductId = 0 OR a.ProductId = @ProductId)
+			AND (@ProductTypeId = 0 OR a.ProductTypeId = @ProductTypeId)
+		GROUP BY pt.ProductTypeName, p.ProductName
+		ORDER BY pt.ProductTypeName, p.ProductName
+	END
+	ELSE
+	BEGIN
+		SELECT v.VenueName, pt.ProductTypeName, p.ProductName, SUM(a.Quantity) AS TotalSales
+		FROM dbo.Activity a
+		INNER JOIN dbo.Product p ON a.ProductId = p.Id
+		INNER JOIN dbo.ProductType pt ON a.ProductTypeId = pt.Id
+		INNER JOIN dbo.Venue v ON a.VenueId = v.Id
+		WHERE	a.Deleted = 0
+			AND	a.ActionId = 5 -- Sale
+			AND (@VenueId = 0 OR a.VenueId = @VenueId)
+			AND (@ProductId = 0 OR a.ProductId = @ProductId)
+			AND (@ProductTypeId = 0 OR a.ProductTypeId = @ProductTypeId)
+		GROUP BY v.VenueName, pt.ProductTypeName, p.ProductName
+		ORDER BY v.VenueName, pt.ProductTypeName, p.ProductName
+	END
 
 	SET @Err = @@Error
 
--- StockManagement.Database/dbo/Stored Procedures/Report_Stock.sql
+++ StockManagement.Database/dbo/Stored Procedures/Report_Stock.sql
@@ -4,6 +4,8 @@
 -- Description:	Get Stock levels
 -- =========================================================
 -- 25 JUN 2025 - Dave Brown - Do not display zero TotalQuantity
+-- 29 JUN 2025 - Dave Brown - Include 'Totals' option
+-- =========================================================
 CREATE PROCEDURE [dbo].[Report_Stock]
 	@VenueId int = 0,
 	@ProductId int = 0,
@@ -13,7 +15,38 @@ BEGIN
 	SET NOCOUNT ON
 	DECLARE @Err int;
 
-    IF @VenueId = 0 -- No venue selected
+    IF @VenueId = -1 -- Totals (not grouped by Venue)
+    BEGIN
+        WITH CombinedActivity AS (
+            SELECT a1.ActivityDate, a1.ProductId, a1.ProductTypeId, a1.VenueId, a1.Quantity * a2.Direction AS Quantity
+            FROM dbo.Activity a1
+	        INNER JOIN dbo.[Action] a2 ON a1.ActionId = a2.Id 
+            WHERE a1.Deleted = 0
+                --AND (@VenueId = 0 OR a1.VenueId = @VenueId)
+                AND (@ProductId = 0 OR a1.ProductId = @ProductId)
+                AND (@ProductTypeId = 0 OR a1.ProductTypeId = @ProductTypeId)
+            UNION ALL
+            SELECT a.ActivityDate, a.ProductId, a.ProductTypeId, 1 /* Stockroom */ as VenueId, a.Quantity * act.Direction * -1 AS Quantity
+            FROM dbo.[Activity] a
+            INNER JOIN dbo.[Action] act ON a.ActionId = act.Id
+            WHERE act.AffectStockRoom = 1
+                AND a.Deleted = 0
+                --AND (@VenueId = 0 OR a.VenueId = @VenueId)
+                AND (@ProductId = 0 OR a.ProductId = @ProductId)
+                AND (@ProductTypeId = 0 OR a.ProductTypeId = @ProductTypeId)
+        )
+
+        -- Perform the GROUP BY on the combined results
+        SELECT -1 AS VenueId, 'Totals' AS VenueName, pt.Id AS ProductTypeId, pt.ProductTypeName, p.Id AS ProductId, p.ProductName, SUM(ca.Quantity) AS TotalQuantity
+        FROM CombinedActivity ca
+        INNER JOIN dbo.Product p ON ca.ProductId = p.Id
+        INNER JOIN dbo.ProductType pt ON ca.ProductTypeId = pt.Id
+        --INNER JOIN dbo.Venue v ON ca.VenueId = v.Id
+        GROUP BY pt.Id, pt.ProductTypeName, p.Id, p.ProductName
+        HAVING SUM(ca.Quantity) > 0
+        ORDER BY pt.ProductTypeName, p.ProductName
+    END
+    ELSE IF @VenueId = 0 -- No venue selected
     BEGIN
         WITH CombinedActivity AS (
             SELECT a1.ActivityDate, a1.ProductId, a1.ProductTypeId, a1.VenueId, a1.Quantity * a2.Direction AS Quantity
